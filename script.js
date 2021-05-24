969e0299c11339e5d95279e1a1f970b72785b860
JJLindsey <jlindsey010@gmail.com>
2021-05-24 17:48:35 -0500

    add javascript var

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 //api call for weather ??
-//api.openweathermap.org/data/2.5/weather?id={city id}&appid={0baea045fada1a05a3ef777664d6c3d3}
-
+//let weatherData = "https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={0baea045fada1a05a3ef777664d6c3d3&q=" + city";
+var ApiKey = "0baea045fada1a05a3ef777664d6c3d3";
 //form variables
 var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city");
 
+//get current weather by City unique Id
+//5 day forecast/future conditions call is displayed 
+function runWeather() {
+
+    const inputEl = document.getElementById("city-search");
+    //element to search for history
+    const searchEl = document.getElementById("search-btn");
+    //button to clear history
+    const clearEl = document.getElementById("clear-btn");
+    const cityNameEl = document.getElementById("city-name");
+    const weatherIcon = document.getElementById("icon");
+    
+
+}
+
+//display current temp
 var getCityData = function(data) {
     var apiWeather = "http://api.openweathermap.org" + data;
 
