e7ae41add35b8bf3b9abc1021273197bdcd06657
JJLindsey <jlindsey010@gmail.com>
2021-05-25 14:16:39 -0500

    add jquery and const

--- script.js
+++ script.js
@@ -3,38 +3,74 @@
 var ApiKey = "0baea045fada1a05a3ef777664d6c3d3";
 //form variables
 var cityFormEl = document.querySelector("#city-form");
-var cityInputEl = document.querySelector("#city");
+var cityInputEl = document.querySelector("#city-search");
 
 //get current weather by City unique Id
 //5 day forecast/future conditions call is displayed 
-function runWeather() {
+//display current date, temp, wind, humidity, UV index and city name using server APIs
+function runWeather(data) {
 
-    const inputEl = document.getElementById("city-search");
+    //const inputEl = document.getElementById("city-search");
+    const inputEl = $("#city-search");
     //element to search for history
-    const searchEl = document.getElementById("search-btn");
+    //const searchEl = document.getElementById("search-btn");
+    const searchEl = $("#search-btn");
     //button to clear history
-    const clearEl = document.getElementById("clear-btn");
-    const cityNameEl = document.getElementById("city-name");
-    const weatherIcon = document.getElementById("icon");
+    //const clearEl = document.getElementById("clear-btn");
+    const clearEl = $("#clear-btn");
+    //const cityNameEl = document.getElementById("city-name");
+    const cityNameEl = $("#city-name");
+    //const weatherIcon = document.getElementById("icon");
+    const weatherIcon = $("#icon");
+    // const tempEl = document.getElementById("temperature");
+    const tempEl = $('#temperature');
+    tempEl.text("temperature: " + data.main.temp);
+
     
+    const windEl = $("#wind-speed");
+    windEl.text("wind-speed: " + data.wind.speed);
+    const humidityEl = $("#humidity");
+    
+    const uvEl = $("#UV-index");
+    // tempEl.textContent = "New value"
+    
+
+
 
 }
 
 //display current temp
 var getCityData = function(data) {
-    var apiWeather = "http://api.openweathermap.org" + data;
+    const apiWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + data + "&appid=0baea045fada1a05a3ef777664d6c3d3";
 
+    // JS fetch vs ajax
     //make a request to url
-    fetch(apiWeather).then(function(response){
-        response.json().then(function(weather){
-            console.log(weather);
-        });
-    });
+    //fetch(apiWeather).then(function(response){
+    //     console.log(response);
+        //response.json().then(function(weather){
+    //         console.log(weather);
+    //     });
+    // });
+
+    // $.ajax({
+    //     url: apiWeather,
+    //     success: function(data) {
+    //         console.log(data)
+    //     }, 
+    // })
+
+    $.get(apiWeather, function(data) {
+        console.log(data)
+
+        // start rendering data
+        runWeather(data)
+
+    })
 };
 
 //get value from city input
 var formSubmitHandler = function(event) {
-
+    event.preventDefault();
     //get value from input element
 var city = cityInputEl.value.trim();
 
